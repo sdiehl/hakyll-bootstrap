85187ef6dd8a4a9bd22592f28f4d70422aa8d2c4
Olivier Verdier <olivier.verdier@gmail.com>
2017-11-23 12:20:44 +0100

    fix discontinued mathjax CDN
    see https://www.mathjax.org/cdn-shutting-down/

--- Main.hs
+++ Main.hs
@@ -32,7 +32,7 @@ mathCtx = field "mathjax" $ \item -> do
   metadata <- getMetadata $ itemIdentifier item
   return ""
   return $ if isJust $ lookupString "mathjax" metadata
-           then "<script type=\"text/javascript\" src=\"http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"></script>"
+           then "<script type=\"text/javascript\" src=\"https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_HTML\"></script>"
            else ""
 
 archiveCtx posts =
